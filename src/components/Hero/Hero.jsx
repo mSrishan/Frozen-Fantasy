9bf2662b680ae65d27d9880c767e2bc9807b8f6a
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-08-10 00:20:06 +0530

    complete the here section structure.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -35,7 +35,22 @@ const Hero = () => {
             </div>
             {/* Hero Image section */}
             <div className='relative'>
-              <img src={iceCorn} alt='ice-cream' className='relative z-40 h-[700px] w-[405px] md-h-[700px] img-shadow'   />
+              <img src={iceCorn} alt='ice-cream' className='relative z-40 h-[700px] w-[405px] md-h-[700px] img-shadow '   />
+            </div>
+            
+             <div className='text-white '>
+              <div className='relative text-white mt-[100px] md:mt-20 p-4 space-y-4'>
+                <div className='relative z-10 space-y-4 ml-14'>
+                  <h1 className='text-3xl'>
+                     The Timeless Charm of Vanilla Ice Cream
+                  </h1>
+                  <h1 className='text-sm opacity-65 leading-loose'>Vanilla ice cream, with its classic and comforting flavor, is a staple in the world of desserts. Whether served simply in a cone or paired with warm pie, its creamy texture and subtle sweetness offer a perfect canvas for a variety of toppings and combinations. 
+                  </h1>
+                </div>
+                <div className='absolute -top-6 -right-8 w-[330px] h-[230px] bg-blue-100/25 '>
+
+                </div>
+              </div>
             </div>
             {/* Third div section */}
             <div></div>
